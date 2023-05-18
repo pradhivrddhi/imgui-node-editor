3a48154d4b609d1380fa95f7021aa31edb149c77
Nitin Aggarwal <nitin.a@pradhivrddhi.in>
2023-05-18 05:34:30 +0530

    Adding premake.lua

--- premake.lua
+++ premake.lua
@@ -1,42 +1,39 @@
 project "ImGuiNodeEditor"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	staticruntime "off"
 
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-        "Examples/Common/BlueprintUtilities/Include/ax/**.h",
-        "Examples/Common/BlueprintUtilities/Include/ax/**.ini",
-        "Examples/Common/BlueprintUtilities/Source/ax/**.cpp",
-        "Examples/Common/BlueprintUtilities/Source/**.h",
-        "Examples/Common/NodeEditor/Include/**.h",
-        "Examples/Common/NodeEditor/Source/**.h",
-        "Examples/Common/NodeEditor/Source/**.cpp",
-        "Examples/Common/NodeEditor/Source/**.ini",
-        "Examples/Common/NodeEditor/Support/imgui_node_editor.natvis",
+		"Examples/Common/BlueprintUtilities/Include/ax/**.h",
+		-- "Examples/Common/BlueprintUtilities/Include/ax/**.ini",
+		"Examples/Common/BlueprintUtilities/Source/ax/**.cpp",
+		"Examples/Common/BlueprintUtilities/Source/**.h",
+		"Examples/Common/NodeEditor/Include/**.h",
+		"Examples/Common/NodeEditor/Source/**.h",
+		"Examples/Common/NodeEditor/Source/**.cpp",
+		-- "Examples/Common/NodeEditor/Source/**.ini",
+		-- "Examples/Common/NodeEditor/Support/imgui_node_editor.natvis",
 	}
 
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-
 	filter "configurations:Debug"
+		defines "PY_DEBUG"
 		runtime "Debug"
-		symbols "on"
+		symbols "On"
 
 	filter "configurations:Release"
+		defines "PY_RELEASE"
 		runtime "Release"
-		optimize "on"
+		optimize "On"
 
-    filter "configurations:Dist"
+	filter "configurations:Dist"
+		defines "PY_DIST"
 		runtime "Release"
-		optimize "on"
-        symbols "off"
+		optimize "On"
